1b02f6a1f0ede6a0d5eec10c45b2cebd9457cfa7
xtratrestrial <travisssmelser@gmail.com>
2024-10-20 14:13:37 -0500

    beginning work on menus, created menus directory

---
+++ start_game.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run the main menu script with Python using the full path
+python3 ~/Xaldrithar-game/menus/main_menu.py
--- utils.py
+++ utils.py
@@ -6,39 +6,64 @@ init(autoreset=True)
 class Formatter:
 
   def red(self,text):
-    print(f"{Fore.RED}{text}{Style.RESET_ALL}")
+    return f"{Fore.RED}{text}{Style.RESET_ALL}"
 
   def green(self,text):
-    print(f"{Fore.GREEN}{text}{Style.RESET_ALL}")
+    return f"{Fore.GREEN}{text}{Style.RESET_ALL}"
 
   def yellow(self,text):
-    print(f"{Fore.YELLOW}{text}{Style.RESET_ALL}")
+    return f"{Fore.YELLOW}{text}{Style.RESET_ALL}"
 
   def cyan(self,text):
-    print(f"{Fore.CYAN}{text}{Style.RESET_ALL}")
+    return f"{Fore.CYAN}{text}{Style.RESET_ALL}"
 
   def magenta(self,text):
-    print(f"{Fore.MAGENTA}{text}{Style.RESET_ALL}")
+    return f"{Fore.MAGENTA}{text}{Style.RESET_ALL}"
 
   def white(self,text):
-    print(f"{Fore.WHITE}{text}{Style.RESET_ALL}")
-
-  def dim(self,text):
-    print(f"{Style.DIM}{text}{Style.RESET_ALL}")
-
-  def bright(self,text):
-    print(f"{Style.BRIGHT}{text}{Style.RESET_ALL}")
+    return f"{Fore.WHITE}{text}{Style.RESET_ALL}"
 
   def normal(self,text):
-    print(f"{Style.NORMAL}{text}{Style.RESET_ALL}")
+    return f"{Style.NORMAL}{text}{Style.RESET_ALL}"
+
+# Brights
+
+  def bright(self,text):
+    return f"{Style.BRIGHT}{text}{Style.RESET_ALL}"
 
   def br_yellow(self,text):
-    print(f"{Fore.YELLOW}{Style.BRIGHT}{text}{Style.RESET_ALL}")
+    return f"{Fore.YELLOW}{Style.BRIGHT}{text}{Style.RESET_ALL}"
 
   def br_red(self,text):
-    print(f"{Fore.RED}{Style.BRIGHT}{text}{Style.RESET_ALL}")
+    return f"{Fore.RED}{Style.BRIGHT}{text}{Style.RESET_ALL}"
+
+# Dims
+
+  def dim(self,text):
+    return f"{Style.DIM}{text}{Style.RESET_ALL}"
+
+  def dim_green(self,text):
+    return f"{Fore.GREEN}{Style.DIM}{text}{Style.RESET_ALL}"
 
 
 # If colored backgrounds are wanted later, just use Back.RED etc.
 
 fmt = Formatter()
+
+# clear screen
+
+# import only system from os
+from os import system, name
+
+# define our clear function
+def clear():
+
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+
